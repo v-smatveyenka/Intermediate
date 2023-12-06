1d90396b3f175837b12242ebb2eedcd9f3ecfd15
Sergey Matveyenko <siarhei_matveyenka@epam.com>
2023-12-07 02:28:50 +0300

    06.ExpressionTrees: ExpressionsTransformer

--- 06.ExpressionTrees/ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
+++ 06.ExpressionTrees/ExpressionTrees.Task1.ExpressionsTransformator/Program.cs
@@ -7,19 +7,43 @@
  * The results could be printed in console or checked via Debugger using any Visualizer.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
-namespace ExpressionTrees.Task1.ExpressionsTransformer
+namespace ExpressionTrees.Task1.ExpressionsTransformer;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Expression Visitor for increment/decrement.");
-            Console.WriteLine();
+        Console.WriteLine("Expression Visitor for increment/decrement.");
+        Console.WriteLine();
 
-            // todo: feel free to add your code here
+        Expression<Func<int, int, int, int>> sourceExpression = (param1, param2, param3) => (param1 + 1) * (param2 - 1) - (param3 + 1);
+        Console.WriteLine($"Source Expression: {sourceExpression}");
 
-            Console.ReadLine();
-        }
+        var incDecExpressionVisitor = new IncDecExpressionVisitor();
+        var result = incDecExpressionVisitor.Visit(sourceExpression);
+        Console.WriteLine($"Result of source expression: {result}\n");
+
+        var parameterReplacerVisitor = new ParameterReplacerVisitor(
+            new Dictionary<string, int>()
+            {
+                { "param1", 5 },
+                { "param2", 7 },
+                { "param3", 17 },
+            }
+        );
+
+        var convertedExpression = parameterReplacerVisitor.Visit(sourceExpression);
+        Console.WriteLine($"Converted expression: {convertedExpression}");
+
+        result = incDecExpressionVisitor.Visit(convertedExpression);
+        Console.WriteLine($"Result of source expression: {result}\n");
+
+        var compiled = ((Expression<Func<int>>)convertedExpression).Compile();
+        Console.WriteLine($"Result: {compiled()}");
+
+        Console.ReadLine();
     }
 }
